573fad1b44417d5fed995f9605272d1b71605b3c
Utsav Ojha <utsavojha2@gmail.com>
2021-02-08 20:45:48 +0545

    routing change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
        <Navbar />
         <Switch>
-           <Route exact path="/">
+           <Route exact path="/cocktailAPI">
             <Home />
            </Route>
            <Route path="/about">
